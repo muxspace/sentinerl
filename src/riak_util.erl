aae0ab8340cdc4765775f010700b188d2cc267b5
Brian Rowe <rowe@muxspace.com>
2011-08-04 14:25:08 -0400

    Coffee....

---
+++ src/riak_util.erl
@@ -0,0 +1,43 @@
+%% Author: Brian Lee Yung Rowe
+%% Created: 2011.07.19
+-module(riak_util).
+-include("private_macros.hrl").
+-compile(export_all).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EXPORTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+new_connection() ->
+  {ok, App} = application:get_application(),
+  new_connection(App).
+
+new_connection(App) ->
+  ?verbose("Using application ~p", [App]),
+  riakc_pb_socket:start_link(get_host(App),get_port(App),get_options()).
+
+
+
+get_host() ->
+  {ok, App} = application:get_application(),
+  get_host(App).
+
+get_host(App) ->
+ case application:get_env(App, riak_host) of
+    undefined -> ?info("Using default host 127.0.0.1"), "127.0.0.1";
+    {ok,H} -> H
+  end.
+
+get_port() ->
+  {ok, App} = application:get_application(),
+  get_port(App).
+
+get_port(App) ->
+  case application:get_env(App, riak_port) of
+    undefined -> ?info("Using default PB port 8081"), 8081;
+    {ok,H} -> H
+  end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PRIVATE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+get_options() ->
+  [ {queue_if_disconnected,true} ].
+
+
